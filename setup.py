1fcfa1d2f4627dba45c6c333f50ba277d7db3c83
Souheil CHELFOUH <trollfot@gmail.com>
2015-01-19 11:17:25 +0100

    initial push

---
+++ setup.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2008-2013 Infrae. All rights reserved.
+# See also LICENSE.txt
+
+from setuptools import setup, find_packages
+import os
+
+version = '1.0.dev0'
+
+tests_require = [
+    ]
+
+
+setup(name='nva.captchawidget',
+      version=version,
+      description="Captcha Widget for UVCSite",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Framework :: Zope2",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: BSD License",
+        ],
+      keywords='captcha',
+      author='Christian Klinger',
+      author_email='ck@novareto.de',
+      url='',
+      license='BSD',
+      package_dir={'': 'src'},
+      packages=find_packages('src'),
+      namespace_packages=['nva'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'skimpyGimpy',
+        'zeam.form.base',
+        'zeam.form.ztk',
+        'zope.component',
+        'zope.interface',
+        'zope.schema',
+        'zope.traversing',
+        ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [zeam.form.components]
+      captcha = nva.captchawidget.widget:register
+      """,
+      tests_require = tests_require,
+      extras_require = {'test': tests_require},
+      )
